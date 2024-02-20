4491aeaec11552f7d8f0f1fcd61d9d199c69cdbd
pouria pou <pouriap024@yahoo.com>
2024-02-20 13:48:37 +0100

    style errorPage & aboutPage

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,12 +1,15 @@
-import ErrorPicture from "../assets/errorPage-picture.webp"
-
-
+import ErrorPicture from "../assets/errorPage-picture.webp";
+import "./errorPage.css";
 const ErrorPage = () => {
   return (
-    <div>
-      <h1>404 Error</h1>
-      <p>Oops! Something went wrong.</p>
-      <img src={ErrorPicture} alt="Error-Image" />
+    <div className="errorPageWrapper">
+      <div className="errorPageDataContainer">
+        <h1>404 Error</h1>
+        <p>Oops! Something went wrong.</p>
+      </div>
+      <div className="errorPageImgContainer">
+        <img src={ErrorPicture} alt="Error-Image" />
+      </div>
     </div>
   );
 };
